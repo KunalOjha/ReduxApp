e2f4a75d397aee8bb82d36aa1609c151331271c5
Kunal Ojha <kunalojha@Kunals-MacBook-Pro.local>
2017-11-27 01:17:21 -0500

    adding first reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Artemis Fowl'},
+        {title: 'Harry Potter'},
+        {title: 'Lord of The Rings'},
+        {title: 'Game of Thrones'}
+    ]
+}
